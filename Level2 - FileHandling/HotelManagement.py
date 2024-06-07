9241cbbe6e9c8da7d5feb6805925658c4320117f
Saksham Dixit <engineersaksham123@gmail.com>
2024-06-07 09:18:39 +0530

    Making Bill format

--- Level2 - FileHandling/HotelManagement.py
+++ Level2 - FileHandling/HotelManagement.py
@@ -1,4 +1,5 @@
 import os
+
 logged_in_admin = None
 current_customer = None
 filepath = "Level2 - FileHandling"
@@ -7,7 +8,7 @@ admin_filename = os.path.join(filepath, "admins.txt")
 
 def add_admin(username, password):
     with open(admin_filename, "a") as f:
-        f.write(username+","+password+"\n")
+        f.write(username + "," + password + "\n")
         print("Username and password are append successfully!")
 
 
@@ -23,18 +24,18 @@ def read_userpass(username, password):
 def adding_prices(customer_name, items, prices):
     customer_file = os.path.join(filepath, customer_name, "Prices.txt")
     with open(customer_file, "a") as file:
-        file.write(items+":$"+str(prices)+"\n")
-        print(f"Added{items} with price ${prices} to {customer_name}'s file.")
+        file.write(items + ":$" + str(prices) + "\n")
+        print(f"Added {items} with price ${prices} to {customer_name}'s file.")
 
 
 def append_cus_data(name, address, contact):
     global filepath
     while os.path.exists(os.path.join(filepath, name)):
         name = input(
-            "The Name you Entered already exists. Enter a different Name :")
+            "The Name you Entered already exists. Enter a different Name: ")
     os.mkdir(os.path.join(filepath, name))
     with open(os.path.join(filepath, name, "customers.txt"), "w") as file:
-        file.write(name+","+address+","+contact+"\n")
+        file.write(name + "," + address + "," + contact + "\n")
         print("Customer Details successfully entered in system")
         global current_customer
         current_customer = name
@@ -44,13 +45,69 @@ def clearConsole():
     return os.system('clear')
 
 
-def show_history(customer):
-    print("---Customer History---")
-    customer_file = os.path.join(filepath, customer, "Prices.txt")
+def print_formatted_history(customer):
+    customer_file = os.path.join(filepath, customer, "customers.txt")
+    price_file = os.path.join(filepath, customer, "Prices.txt")
+
     with open(customer_file) as file:
-        lines = file.readlines()
-        for line in lines:
-            print(line.strip())
+        line = file.readline()
+        name, address, contact = line.strip().split(',')
+
+    print(f"Name: {name}")
+    print(f"Address: {address}")
+    print(f"Phone Number: {contact}")
+    print("--------")
+    print("INVOICE")
+    print("--------")
+
+    booking_items = ["Ultra Royal", "Royal", "Elite", "Common"]
+    gaming_items = ["Carrom", "Chess", "Table Tennis"]
+    restaurant_items = ["Pizza", "Burger", "Salad"]
+
+    booking_bill = []
+    gaming_bill = []
+    restaurant_bill = []
+
+    total_amount = 0
+
+    with open(price_file) as file:
+        for line in file:
+            item, price = line.strip().split(":$")
+            price = int(price)
+            total_amount += price
+            if item in booking_items:
+                booking_bill.append(f"{item}: ${price}")
+            elif item in gaming_items:
+                gaming_bill.append(f"{item}: ${price}")
+            elif item in restaurant_items:
+                restaurant_bill.append(f"{item}: ${price}")
+
+    if booking_bill:
+        print("Booking Bill")
+        for item in booking_bill:
+            print(item)
+
+    if gaming_bill:
+        print("\nGaming Bill")
+        for item in gaming_bill:
+            print(item)
+
+    if restaurant_bill:
+        print("\nRestaurant Bill")
+        for item in restaurant_bill:
+            print(item)
+    print("~~~~~~")
+    print("Total")
+    print("~~~~~~")
+    print(f"Grand Bill\nTotal: ${total_amount}")
+
+
+def admin_show_history(customer):
+    print_formatted_history(customer)
+
+
+def show_history(customer):
+    print_formatted_history(customer)
 
 
 def register_admin():
@@ -96,13 +153,13 @@ def show_menu():
     print("2. Restaurant Area")
     print("3. Gaming Zone")
     print("4. Calculate Total Bill")
-    print("5. Show History")
+    print("5. Show Transaction History for current user")
     print("6. Exit")
 
 
 def booking_record(customer):
     print("\n--- Booking Record ---")
-    print("1. Ultra Royal - $ 5000")
+    print("1. Ultra Royal - $5000")
     print("2. Royal - $3500")
     print("3. Elite - $2500")
     print("4. Common - $1500")
@@ -110,11 +167,11 @@ def booking_record(customer):
     choice = int(input("Enter your choice: "))
     prices = [5000, 3500, 2500, 1500]
     items = ["Ultra Royal", "Royal", "Elite", "Common"]
-    if 0 <= choice < len(prices):
-        adding_prices(customer, items[choice-1], prices[choice-1])
+    if 1 <= choice <= len(prices):
+        adding_prices(customer, items[choice - 1], prices[choice - 1])
         print("Item added to your bill.")
     else:
-        print()
+        print("Invalid choice! Please try again.")
 
 
 def restaurant_area(customer):
@@ -126,11 +183,11 @@ def restaurant_area(customer):
     choice = int(input("Enter your choice: "))
     prices = [10, 7, 5]
     items = ["Pizza", "Burger", "Salad"]
-    if 0 <= choice < len(prices):
-        adding_prices(customer, items[choice-1], prices[choice-1])
+    if 1 <= choice <= len(prices):
+        adding_prices(customer, items[choice - 1], prices[choice - 1])
         print("Item added to your bill.")
     else:
-        print()
+        print("Invalid choice! Please try again.")
 
 
 def gaming_zone(customer):
@@ -141,25 +198,16 @@ def gaming_zone(customer):
     print("4. Go to Main Menu")
     choice = int(input("Enter your choice: "))
     prices = [5, 3, 8]
-    items = ["Carrom", "chese", "Table Tennis"]
-    if 0 <= choice < len(prices):
-        adding_prices(customer, items[choice-1], prices[choice-1])
+    items = ["Carrom", "Chess", "Table Tennis"]
+    if 1 <= choice <= len(prices):
+        adding_prices(customer, items[choice - 1], prices[choice - 1])
         print("Game added to your bill.")
     else:
-        print()
+        print("Invalid choice! Please try again.")
 
 
 def calculate_total_bill(customer):
-    print("---Total Bill---")
-    customer_file = os.path.join(filepath, customer, "Prices.txt")
-    total_bill = 0
-    with open(customer_file, "r")as file:
-        lines = file.readlines()
-        for line in lines[1:]:
-            items, prices = line.split(":$")
-            total_bill += int(prices.strip())
-    print(f"Customer Name :{customer}")
-    print(f"Total Bill :${total_bill}")
+    print_formatted_history(customer)
 
 
 def handle_customer(customer):
@@ -178,6 +226,7 @@ def handle_customer(customer):
                 input("Press enter to go back!")
             case '5':
                 show_history(customer)
+                input("Press enter to go back!")
             case '6':
                 print("Exiting customer area...")
                 global current_customer
@@ -205,12 +254,13 @@ while True:
                 print("Invalid choice! Please try again.")
         clearConsole()
     else:
-        if current_customer == None:
+        if current_customer is None:
             print("Welcome,", logged_in_admin)
             print("1. Add a New Customer")
-            print("2. Add a Admin ")
-            print("3. Logout")
-            print("4. Exit")
+            print("2. Add an Admin")
+            print("3. Show Transaction History for a Customer")
+            print("4. Logout")
+            print("5. Exit")
             option = input("Enter your choice: ")
             match option:
                 case '1':
@@ -218,8 +268,12 @@ while True:
                 case '2':
                     register_admin()
                 case '3':
-                    logout_admin()
+                    give_customer = input("Enter the name of the Customer: ")
+                    admin_show_history(give_customer)
+                    input("Press enter to Go back!")
                 case '4':
+                    logout_admin()
+                case '5':
                     print("Exiting Program...")
                     os._exit(1)
                 case _:
